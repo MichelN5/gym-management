cc01529603ef8886d679fd254bf47fcabed24108
Wassim-php <omzznais@gmail.com>
2025-02-06 14:19:57 +0200

    initial commit

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -10,7 +10,7 @@ const PrivateRoute = ({ children, roleRequired }) => {
 
     }
 
-    if (roleRequired || userRole !== roleRequired) {
+    if (roleRequired && userRole !== roleRequired) {
         return <Navigate to="/" />; // Redirect to home or another page
     }
 
